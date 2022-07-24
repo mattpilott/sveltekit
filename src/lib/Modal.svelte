6338c062bf854470bb64322fef80f2ed2116a884
Matt Pilott <hello@matt-pilott.com>
2022-07-24 13:52:57 +0100

    initial

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,4 @@
+<div style="padding: 100px;border:1px;">
+   Hello I am the modal
+   <a href="/">close me</a>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+   import { page } from '$app/stores'
+   import Modal from '$lib/Modal.svelte'
+</script>
+
+<a href="/#modal">Open Modal</a>
+
+{#if $page.url.hash === '#modal'}
+   <Modal />
+{/if}
